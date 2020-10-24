e86a3b1650c8751e9ce2962572a8ba2e6064a4f5
Francisco Lopez <franlopbri@gmail.com>
2020-10-24 14:16:13 -0500

    instala jdk de auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-vuisvhqz.auth0.com',
+      clientId: 'nWFNcQDO2ijUjINEJ4NDjm0NbIFLDyAJ'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
